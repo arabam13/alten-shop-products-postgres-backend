2dc9c2c79b997658a9016c1d33974901548b589c
araba <araba.mohamed@gmail.com>
2024-06-07 01:30:49 +0200

    updating server.js

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ server.on("error", errorHandler);
 server.on("listening", () => {
   const address = server.address();
   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-  // console.log("Listening on " + bind);
+  console.log("Adress " + address);
+  console.log("Listening on " + bind);
   // console.log(
   //   "Swagger link: " + "http://localhost:" + bind.split(" ")[1] + "/api-docs"
   // );
